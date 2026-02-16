a49ef9bc1e4f64377bedef0ffafb7be8d37f3f0e
Luke Climenhage <luke.climenhage@gmail.com>
2026-02-15 21:13:17 -0800

    Add tailwind to project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://lukeclimen.github.io",
+  integrations: [tailwind()],
 });
